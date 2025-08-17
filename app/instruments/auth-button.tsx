051e65169bbecf63c2ac8fa9ecceebb1ed0a96d9
James <james1111a@proton.me>
2025-08-17 19:00:13 +0800

    feature: github 0auth demo

---
+++ app/instruments/auth-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { createClient } from "@/lib/supabase/client";
+
+export default function AuthButton() {
+  const supabase = createClient();
+  async function handleSingIn() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `http://localhost:3000/auth/callback?next=/instruments`,
+      },
+    });
+  }
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("logout error:", error);
+    } else {
+      console.log("logout success");
+    }
+  }
+  return (
+    <>
+      <button onClick={handleSingIn}>login</button>
+      <button onClick={signOut}>logout</button>
+    </>
+  );
+}
--- app/instruments/page.tsx
+++ app/instruments/page.tsx
@@ -1,8 +1,14 @@
 import { createClient } from "@/lib/supabase/server";
+import AuthButton from "./auth-button";
 
 export default async function Instruments() {
   const supabase = await createClient();
   const { data: instruments } = await supabase.from("tweets").select();
 
-  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+  return (
+    <>
+      <AuthButton />
+      <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+    </>
+  );
 }
